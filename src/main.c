f2df8128822ae59e9517c6b63ec0b93dd6c25e0c
Deomid Ryabkov <rojer@cesanta.com>
2018-10-12 18:11:32 +0100

    Allow RPC ID to be string
    CL: rpc: Allow RPC ID to be string
    
    PUBLISHED_FROM=edfa36dfd2943cc14ed55dec0934fadc8de6d1fb

--- src/main.c
+++ src/main.c
@@ -78,8 +78,9 @@ static void button_cb(int pin, void *arg) {
                             (unsigned long) mgos_get_heap_size(),
                             (unsigned long) mgos_get_free_heap_size());
   char buf[8];
-  LOG(LL_INFO,
-      ("Pin: %s, published: %s", mgos_gpio_str(pin, buf), res ? "yes" : "no"));
+  int x = mgos_gpio_toggle(mgos_sys_config_get_board_led3_pin());
+  LOG(LL_INFO, ("Pin: %s, published: %s x %d", mgos_gpio_str(pin, buf),
+                res ? "yes" : "no", x));
   (void) arg;
 }
 
@@ -92,6 +93,9 @@ enum mgos_app_init_result mgos_app_init(void) {
     mgos_gpio_set_mode(led_pin, MGOS_GPIO_MODE_OUTPUT);
     mgos_set_timer(1000, MGOS_TIMER_REPEAT, led_timer_cb, NULL);
   }
+  mgos_gpio_set_mode(mgos_sys_config_get_board_led3_pin(),
+                     MGOS_GPIO_MODE_OUTPUT);
+  mgos_gpio_write(mgos_sys_config_get_board_led3_pin(), 0);
 
   /* Publish to MQTT on button press */
   int btn_pin = mgos_sys_config_get_board_btn1_pin();
